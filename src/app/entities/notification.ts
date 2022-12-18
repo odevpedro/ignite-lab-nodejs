b5686207f0fd92a3aa1d0166fc2e72848f8fe44f
odevepdro <pedrosschmidt2@gmail.com>
2022-12-18 20:51:43 -0300

    teste de criação de notificação

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -1,3 +1,4 @@
+import { Replace } from 'src/helpers/replace';
 import { Content } from './content';
 
 export interface NotificationProps {
@@ -13,8 +14,11 @@ export class Notification {
 
   private props: NotificationProps;
 
-  constructor(props: NotificationProps) {
-        this.props = props;
+  constructor(props: Replace<NotificationProps, {createdAt?: Date}>) {
+        this.props = {
+            ...props,
+            createdAt: props.createdAt ?? new Date(),
+        };
     }
 
     public set content(content: Content) {
